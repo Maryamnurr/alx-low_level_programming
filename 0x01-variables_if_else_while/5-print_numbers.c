48049f2c43bd70696a73dd529f9c40ce9b5b6452
maryamnurr <abdulmalikmaryam4@gmail.com>
2022-09-09 19:40:06 -0700

    corections

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,6 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
+
 /**
  * main - point all single digit numbers of base 10 starting from 0.
  * Return: Always 0 (success)
@@ -10,7 +9,7 @@ int main(void)
 	int num;
 
 	for (num = 0; num < 10; num++)
-		printf("%d", num)
-			printf("\n");
+		printf("%d", num);
+	printf("\n");
 	return (0);
 }
